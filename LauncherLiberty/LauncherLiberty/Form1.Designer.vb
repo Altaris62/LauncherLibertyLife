8dca1f3b127a9e620f42b85d400e91aad97aa966
Totor <tytusmatyk@gmail.com>
2018-01-05 10:09:31 +0100

    1st time

---
+++ LauncherLiberty/LauncherLiberty/Form1.Designer.vb
@@ -0,0 +1,203 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.FormSkin1 = New WindowsApplication1.FormSkin()
+        Me.FlatButton5 = New WindowsApplication1.FlatButton()
+        Me.FlatButton4 = New WindowsApplication1.FlatButton()
+        Me.FlatButton3 = New WindowsApplication1.FlatButton()
+        Me.FlatButton2 = New WindowsApplication1.FlatButton()
+        Me.FlatButton1 = New WindowsApplication1.FlatButton()
+        Me.FlatProgressBar1 = New WindowsApplication1.FlatProgressBar()
+        Me.FlatMini1 = New WindowsApplication1.FlatMini()
+        Me.FlatClose1 = New WindowsApplication1.FlatClose()
+        Me.FormSkin1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'FormSkin1
+        '
+        Me.FormSkin1.BackColor = System.Drawing.Color.White
+        Me.FormSkin1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(73, Byte), Integer))
+        Me.FormSkin1.BorderColor = System.Drawing.Color.FromArgb(CType(CType(53, Byte), Integer), CType(CType(58, Byte), Integer), CType(CType(60, Byte), Integer))
+        Me.FormSkin1.Controls.Add(Me.FlatButton5)
+        Me.FormSkin1.Controls.Add(Me.FlatButton4)
+        Me.FormSkin1.Controls.Add(Me.FlatButton3)
+        Me.FormSkin1.Controls.Add(Me.FlatButton2)
+        Me.FormSkin1.Controls.Add(Me.FlatButton1)
+        Me.FormSkin1.Controls.Add(Me.FlatProgressBar1)
+        Me.FormSkin1.Controls.Add(Me.FlatMini1)
+        Me.FormSkin1.Controls.Add(Me.FlatClose1)
+        Me.FormSkin1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FormSkin1.FlatColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FormSkin1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FormSkin1.HeaderColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.FormSkin1.HeaderMaximize = False
+        Me.FormSkin1.Location = New System.Drawing.Point(0, 0)
+        Me.FormSkin1.Name = "FormSkin1"
+        Me.FormSkin1.Size = New System.Drawing.Size(658, 261)
+        Me.FormSkin1.TabIndex = 0
+        Me.FormSkin1.Text = "Launcher Liberty-Life"
+        '
+        'FlatButton5
+        '
+        Me.FlatButton5.BackColor = System.Drawing.Color.Transparent
+        Me.FlatButton5.BaseColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FlatButton5.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FlatButton5.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FlatButton5.Location = New System.Drawing.Point(69, 137)
+        Me.FlatButton5.Name = "FlatButton5"
+        Me.FlatButton5.Rounded = False
+        Me.FlatButton5.Size = New System.Drawing.Size(97, 33)
+        Me.FlatButton5.TabIndex = 7
+        Me.FlatButton5.Text = "Teamspeak"
+        Me.FlatButton5.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'FlatButton4
+        '
+        Me.FlatButton4.BackColor = System.Drawing.Color.Transparent
+        Me.FlatButton4.BaseColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FlatButton4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FlatButton4.Font = New System.Drawing.Font("Segoe UI", 10.0!)
+        Me.FlatButton4.Location = New System.Drawing.Point(274, 138)
+        Me.FlatButton4.Name = "FlatButton4"
+        Me.FlatButton4.Rounded = False
+        Me.FlatButton4.Size = New System.Drawing.Size(106, 32)
+        Me.FlatButton4.TabIndex = 6
+        Me.FlatButton4.Text = "Maj TFAR"
+        Me.FlatButton4.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'FlatButton3
+        '
+        Me.FlatButton3.BackColor = System.Drawing.Color.Transparent
+        Me.FlatButton3.BaseColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FlatButton3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FlatButton3.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FlatButton3.Location = New System.Drawing.Point(432, 191)
+        Me.FlatButton3.Name = "FlatButton3"
+        Me.FlatButton3.Rounded = False
+        Me.FlatButton3.Size = New System.Drawing.Size(200, 32)
+        Me.FlatButton3.TabIndex = 5
+        Me.FlatButton3.Text = "Jouer"
+        Me.FlatButton3.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'FlatButton2
+        '
+        Me.FlatButton2.BackColor = System.Drawing.Color.Transparent
+        Me.FlatButton2.BaseColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FlatButton2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FlatButton2.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FlatButton2.Location = New System.Drawing.Point(226, 191)
+        Me.FlatButton2.Name = "FlatButton2"
+        Me.FlatButton2.Rounded = False
+        Me.FlatButton2.Size = New System.Drawing.Size(200, 32)
+        Me.FlatButton2.TabIndex = 4
+        Me.FlatButton2.Text = "Mise à jour"
+        Me.FlatButton2.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'FlatButton1
+        '
+        Me.FlatButton1.BackColor = System.Drawing.Color.Transparent
+        Me.FlatButton1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FlatButton1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FlatButton1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FlatButton1.Location = New System.Drawing.Point(20, 191)
+        Me.FlatButton1.Name = "FlatButton1"
+        Me.FlatButton1.Rounded = False
+        Me.FlatButton1.Size = New System.Drawing.Size(200, 32)
+        Me.FlatButton1.TabIndex = 3
+        Me.FlatButton1.Text = "Forum"
+        Me.FlatButton1.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'FlatProgressBar1
+        '
+        Me.FlatProgressBar1.BackColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(73, Byte), Integer))
+        Me.FlatProgressBar1.DarkerProgress = System.Drawing.Color.FromArgb(CType(CType(23, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(92, Byte), Integer))
+        Me.FlatProgressBar1.Location = New System.Drawing.Point(12, 209)
+        Me.FlatProgressBar1.Maximum = 100
+        Me.FlatProgressBar1.Name = "FlatProgressBar1"
+        Me.FlatProgressBar1.ProgressColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FlatProgressBar1.Size = New System.Drawing.Size(634, 42)
+        Me.FlatProgressBar1.TabIndex = 2
+        Me.FlatProgressBar1.Text = "FlatProgressBar1"
+        Me.FlatProgressBar1.Value = 0
+        '
+        'FlatMini1
+        '
+        Me.FlatMini1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.FlatMini1.BackColor = System.Drawing.Color.White
+        Me.FlatMini1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.FlatMini1.Font = New System.Drawing.Font("Marlett", 12.0!)
+        Me.FlatMini1.Location = New System.Drawing.Point(604, 12)
+        Me.FlatMini1.Name = "FlatMini1"
+        Me.FlatMini1.Size = New System.Drawing.Size(18, 18)
+        Me.FlatMini1.TabIndex = 1
+        Me.FlatMini1.Text = "FlatMini1"
+        Me.FlatMini1.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'FlatClose1
+        '
+        Me.FlatClose1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.FlatClose1.BackColor = System.Drawing.Color.White
+        Me.FlatClose1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(168, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer))
+        Me.FlatClose1.Font = New System.Drawing.Font("Marlett", 10.0!)
+        Me.FlatClose1.Location = New System.Drawing.Point(628, 13)
+        Me.FlatClose1.Name = "FlatClose1"
+        Me.FlatClose1.Size = New System.Drawing.Size(18, 18)
+        Me.FlatClose1.TabIndex = 0
+        Me.FlatClose1.Text = "FlatClose1"
+        Me.FlatClose1.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(658, 261)
+        Me.Controls.Add(Me.FormSkin1)
+        Me.Font = New System.Drawing.Font("Montserrat", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Launcher Liberty-Life"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.FormSkin1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents FormSkin1 As WindowsApplication1.FormSkin
+
+    Private Sub FormSkin1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FormSkin1.Click
+
+    End Sub
+    Friend WithEvents FlatButton2 As WindowsApplication1.FlatButton
+    Friend WithEvents FlatButton1 As WindowsApplication1.FlatButton
+    Friend WithEvents FlatProgressBar1 As WindowsApplication1.FlatProgressBar
+    Friend WithEvents FlatMini1 As WindowsApplication1.FlatMini
+    Friend WithEvents FlatClose1 As WindowsApplication1.FlatClose
+    Friend WithEvents FlatButton3 As WindowsApplication1.FlatButton
+    Friend WithEvents FlatButton4 As WindowsApplication1.FlatButton
+    Friend WithEvents FlatButton5 As WindowsApplication1.FlatButton
+
+
+End Class
---
+++ LauncherLiberty/LauncherLiberty/Form1.vb
@@ -0,0 +1,86 @@
+﻿Imports Microsoft.Win32
+
+Public Class Form1
+    Dim directory As String = False
+
+    Private Sub Unrar(ByVal filepath As String, ByVal WorkingDir As String)
+        Dim objRegKey As RegistryKey
+        objRegKey = Registry.ClassesRoot.OpenSubKey("WinRAR\Shell\Open\Command")
+        Dim obj As Object = objRegKey.GetValue("")
+        Dim objRarPath As String = obj.ToString()
+
+        objRarPath = objRarPath.Substring(1, objRarPath - 7)
+
+        objRegKey.Close()
+
+        Dim objArgs As String
+        objArgs = " X " & " " & filepath & " " + " " + WorkingDir
+
+        Dim objStartInfo As New ProcessStartInfo()
+        objStartInfo.UseShellExecute = False
+        objStartInfo.FileName = objRarPath
+        objStartInfo.Arguments = objArgs
+        objStartInfo.WindowStyle = ProcessWindowStyle.Hidden
+        objStartInfo.WorkingDirectory = WorkingDir & ""
+
+        Dim objPro As New Process()
+        objPro.StartInfo = objStartInfo
+        objPro.Start()
+
+    End Sub
+
+    Private Sub setA3Directory()
+        Dim FolderBrowserDialog As New FolderBrowserDialog
+        FolderBrowserDialog.ShowDialog()
+        directory = FolderBrowserDialog.SelectedPath
+
+        If directory.Equals("") Then
+
+            Exit Sub
+        End If
+
+        My.Computer.FileSystem.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\LauncherLibertyLife")
+        My.Computer.FileSystem.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\LauncherLibertyLife\directory.a3", directory, False)
+
+    End Sub
+
+
+    Private Sub FlatButton1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FlatButton1.Click
+        Process.Start("https://forum.liberty-life.ovh")
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        If My.Computer.FileSystem.FileExists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\LauncherLibertyLife\directory.a3") Then
+            directory = My.Computer.FileSystem.ReadAllText(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\LauncherLibertyLife\directory.a3")
+        Else
+            MsgBox("Vous devez selectionnez le chemin d'accès de votre dossier ARMA III", MsgBoxStyle.Exclamation, "Erreur")
+            setA3Directory()
+        End If
+    End Sub
+
+    Private Sub FlatButton3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FlatButton3.Click
+        If My.Computer.FileSystem.FileExists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\LauncherLibertyLife\directory.a3") Then
+            Process.Start(My.Computer.FileSystem.ReadAllText(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\LauncherLibertyLife\directory.a3") & "\arma3_x64.exe")
+        Else
+            MsgBox("Vous devez selectionnez le chemin d'accès de votre dossier ARMA III", MsgBoxStyle.Exclamation, "Erreur")
+            setA3Directory()
+        End If
+    End Sub
+
+    Private Sub FlatButton2_Click(ByVal sender As Object, ByVal e As EventArgs) Handles FlatButton2.Click
+        If My.Computer.FileSystem.FileExists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\LauncherLibertyLife\directory.a3") Then
+
+        Else
+            MsgBox("Vous devez selectionnez le chemin d'accès de votre dossier ARMA III", MsgBoxStyle.Exclamation, "Erreur")
+            setA3Directory()
+        End If
+    End Sub
+
+    Private Sub FlatButton5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FlatButton5.Click
+
+    End Sub
+
+    Private Sub FlatButton4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FlatButton4.Click
+
+    End Sub
+End Class
